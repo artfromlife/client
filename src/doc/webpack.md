b64c4fa62c7afa9a15fbe64844b2360c733c55ee
zxvf <424160958@qq.com>
2022-10-07 23:28:33 +0800

    splitChunks ?

---
+++ src/doc/webpack.md
@@ -0,0 +1,1 @@
+How to use ``optimization.splitChunks`` ?
--- webpack.config.ts
+++ webpack.config.ts
@@ -8,24 +8,44 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import MonacoEditorWebpackPlugin from 'monaco-editor-webpack-plugin'
 import MiniCssExtractPlugin from "mini-css-extract-plugin"
 import CssMinimizerPlugin from "css-minimizer-webpack-plugin"
-import { CleanWebpackPlugin } from "clean-webpack-plugin"
+import {CleanWebpackPlugin} from "clean-webpack-plugin"
+import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer' // split bundles , not code splitting 先把大的包拆成小的
+
 const config: webpack.Configuration = {
     mode: 'production',
     entry: './main.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[hash:8].js',
-        sourceMapFilename: '[name].[hash:8].map',
-        chunkFilename: '[id].[hash:8].js'
+        filename: '[name].js',
     },
     optimization: {
-        minimize: true, // false 的话 不压缩代码
+        minimize: true, // false 的话 不压缩代码, 内部用的是 TerserPlugin ?
         minimizer: [
+            `...`, // 保留原来压缩JS等文件的能力
             new CssMinimizerPlugin()
         ],
+        // runtimeChunk: 'single',
         splitChunks: {
-            chunks: 'all'
-        }
+            chunks: 'all',
+            minSize: 10 * 1024,
+            minRemainingSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            enforceSizeThreshold: 50000,
+            cacheGroups: {
+                defaultVendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                    reuseExistingChunk: true,
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true,
+                },
+            },
+        },
     },
     module: {
         rules: [
@@ -33,12 +53,7 @@ const config: webpack.Configuration = {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
-            {
-                test: /\.ts$/,
-                use: {
-                    loader: 'ts-loader'
-                }
-            }
+            {test: /\.ts$/, use: {loader: 'ts-loader'}}
         ]
     },
     plugins: [
@@ -46,11 +61,12 @@ const config: webpack.Configuration = {
             template: './public/index.html'
         }), // 该插件将为你生成一个 HTML5 文件， 在 body 中使用 script 标签引入你所有 webpack 生成的 bundle
             // 插入的 script 用的是 defer, 并行下载js资源, 不阻塞且不打断html解析
-        new MonacoEditorWebpackPlugin({
-            languages: ['javascript']
-        }),
+        // new MonacoEditorWebpackPlugin({
+        //     languages: ['javascript']
+        // }),
         new MiniCssExtractPlugin(),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new BundleAnalyzerPlugin()
     ],
     resolve: {
         extensions: ['.ts', '.js', '.json']
@@ -60,5 +76,4 @@ const config: webpack.Configuration = {
     }
 };
 
-console.log('当前环境：' + process.env.NODE_ENV)
 export default config;
